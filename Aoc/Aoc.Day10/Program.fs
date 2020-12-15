76cebfd1b176c32ef840589b682fadd1be0ab655
KiritchoukC <clement.kiritchouk@gmail.com>
2020-12-15 21:43:31 +0100

    refactor(day10): refactor part1

--- Aoc/Aoc.Day10/Program.fs
+++ Aoc/Aoc.Day10/Program.fs
@@ -2,41 +2,47 @@
 open System
 open System.Linq
 
-type AdaptersCount = {
-    Ones: int
-    Twos: int
-    Threes: int
-}
-
 let readLines path = System.IO.File.ReadLines(path).ToArray()
 
-let count (predicate: (int*int list) -> bool) (diff: (int * int list)list) = 
-    let countOption = diff |> List.tryFind predicate
-    match countOption with 
-    | None -> 0
-    | Some x -> x |> snd |> List.length |> (+) 1
-    
-
-let countAdapters (diff: (int * int list)list) = 
-    {
-        Ones    = diff |> count (fun x -> (fst x) = 1)
-        Twos    = diff |> count (fun x -> (fst x) = 2)
-        Threes  = diff |> count (fun x -> (fst x) = 3)
+module part1 =
+    type AdaptersCount = {
+        Ones: int
+        Twos: int
+        Threes: int
     }
 
-let getResult adaptersCount = adaptersCount.Ones * adaptersCount.Threes
+    let count number (diff: (int * int list)list) = 
+        let countOption = diff |> List.tryFind (fun x -> (fst x) = number)
+        match countOption with 
+        | None -> 0
+        | Some x -> x |> snd |> List.length |> (+) 1
+    
+
+    let countAdapters (diff: (int * int list)list) = 
+        {
+            Ones    = diff |> count 1
+            Twos    = diff |> count 2
+            Threes  = diff |> count 3
+        }
+
+    let getResult adaptersCount = adaptersCount.Ones * adaptersCount.Threes
+
+    let solve adapters =
+        adapters
+        |> List.pairwise
+        |> List.map (fun p -> (snd p) - (fst p))
+        |> List.groupBy id
+        |> countAdapters
+        |> getResult
+        
 
 [<EntryPoint>]
 let main argv =
-    "input.txt"
-    |> readLines
-    |> Array.toList
-    |> List.map int
-    |> List.sortBy id
-    |> List.pairwise
-    |> List.map (fun p -> (snd p) - (fst p))
-    |> List.groupBy id
-    |> countAdapters
-    |> getResult
-    |> printf "%A"
+    let input =
+        "input.txt"
+        |> readLines
+        |> Array.toList
+        |> List.map int
+        |> List.sortBy id
+    input |> part1.solve |> printf "%A"
     0
